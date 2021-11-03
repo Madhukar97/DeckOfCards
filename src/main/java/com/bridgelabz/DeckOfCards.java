61249cb878caa2a0e814f2426f978adba76d5618
unknown <madhukar.tatiraju@gmail.com>
2021-11-03 09:26:27 +0530

    deck of cards generated

---
+++ src/main/java/com/bridgelabz/DeckOfCards.java
@@ -0,0 +1,51 @@
+package com.bridgelabz;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class DeckOfCards {
+
+    protected String[] suit = {"Clubs", "Diamonds", "Hearts", "Spades"};
+    protected String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+    protected static String[] array1D = new String[52];
+
+
+    public static void main(String[] args) {
+        DeckOfCards obj = new DeckOfCards();
+        obj.generateRandomCards();
+        Arrays.sort(array1D);
+        obj.showCards();
+    }
+
+    //Method to show the deck of cards
+    public void showCards() {
+        System.out.println("all cards " + Arrays.toString(array1D) + " \n");
+    }
+
+    //Method to generate deck of cards
+    public void generateRandomCards() {
+        for (int i = 0; i < array1D.length; i++) {
+            array1D[i] = "";
+        }
+        DeckOfCards obj = new DeckOfCards();
+        for (int i = 0; i < array1D.length; i++) {
+            String card = randomCard();
+            for (int j = 0; j <= i; j++) {
+                if (array1D[j].equals(card)) {
+                    card = obj.randomCard();
+                    j = 0;
+                }
+            }
+            array1D[i] = card;
+        }
+    }
+
+    /**
+     *
+     * @return
+     */
+    private String randomCard() {
+        Random rand = new Random();
+        return suit[rand.nextInt(4)] + " " + rank[rand.nextInt(13)];
+    }
+}
